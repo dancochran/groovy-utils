ff46227ab1326060951ba66f1a63caf74201f07a
Dan Cochran <daniel.s.cochran@huntington.com>
2018-01-10 15:07:44 -0500

    <source> <target> - copies a source file to target file, with overwrite as needed

---
+++ copy-file-with-overwrite.groovy
@@ -0,0 +1,21 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.NoSuchFileException;
+
+//println args[0]
+//println args[1]
+//println args[2]
+
+Path source = Paths.get(args[0])
+Path target = Paths.get(args[1])
+try
+{
+	Files.delete(target)
+}
+catch (NoSuchFileException nsfex)
+{
+	// no-op, nothing was there to delete, we can copy anyway
+}
+
+Files.copy(source, target)
